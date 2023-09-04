c0094939ea2a5fa3aa4db098441fa559067b4404
Aaron3312 <a01642529@tec.mx>
2023-09-04 11:04:31 -0600

    ConLaOpcionDeRevisionDeSiEstaOrdenada

--- Pruebas_cpp/Algoritmos I/actividades/Act1.3/Act1.3.cpp
+++ Pruebas_cpp/Algoritmos I/actividades/Act1.3/Act1.3.cpp
@@ -21,16 +21,19 @@
 using namespace std;
 
 // Función para combinar dos subvectores ordenados
-void merge( vector<int>& dia, int izq, int medio, int derecha) {
+void merge(vector<int> &dia, int izq, int medio, int derecha)
+{
     int n1 = medio - izq + 1;
     int n2 = derecha - medio;
     vector<int> izqArr(n1);
     vector<int> derechArr(n2);
 
-    for (int i = 0; i < n1; i++) {
+    for (int i = 0; i < n1; i++)
+    {
         izqArr[i] = dia[izq + i];
     }
-    for (int j = 0; j < n2; j++) {
+    for (int j = 0; j < n2; j++)
+    {
         derechArr[j] = dia[medio + 1 + j];
     }
 
@@ -39,11 +42,15 @@ void merge( vector<int>& dia, int izq, int medio, int derecha) {
     int j = 0;
     int k = izq;
 
-    while (i < n1 && j < n2) {
-        if (izqArr[i] <= derechArr[j]) {
+    while (i < n1 && j < n2)
+    {
+        if (izqArr[i] <= derechArr[j])
+        {
             dia[k] = izqArr[i];
             i++;
-        } else {
+        }
+        else
+        {
             dia[k] = derechArr[j];
             j++;
         }
@@ -51,14 +58,16 @@ void merge( vector<int>& dia, int izq, int medio, int derecha) {
     }
 
     // Copiar los elementos restantes de izqArr[], si los hay
-    while (i < n1) {
+    while (i < n1)
+    {
         dia[k] = izqArr[i];
         i++;
         k++;
     }
 
     // Copiar los elementos restantes de derechArr[], si los hay
-    while (j < n2) {
+    while (j < n2)
+    {
         dia[k] = derechArr[j];
         j++;
         k++;
@@ -66,8 +75,10 @@ void merge( vector<int>& dia, int izq, int medio, int derecha) {
 }
 
 // Función recursiva para realizar MergeSort
-void mergeSort(std::vector<int>& dia, int izq, int derecha) {
-    if (izq < derecha) {
+void mergeSort(std::vector<int> &dia, int izq, int derecha)
+{
+    if (izq < derecha)
+    {
         // Encuentra el punto medio
         int medio = izq + (derecha - izq) / 2;
 
@@ -80,66 +91,83 @@ void mergeSort(std::vector<int>& dia, int izq, int derecha) {
     }
 }
 
-int numeroMes(string mes){
-    if (mes == "Jan"){
+int numeroMes(string mes)
+{
+    if (mes == "Jan")
+    {
         return 1;
     }
-    if (mes == "Feb"){
+    if (mes == "Feb")
+    {
         return 2;
     }
-    if (mes == "Mar"){
+    if (mes == "Mar")
+    {
         return 3;
     }
-    if (mes == "Apr"){
+    if (mes == "Apr")
+    {
         return 4;
     }
-    if (mes == "May"){
+    if (mes == "May")
+    {
         return 5;
     }
-    if (mes == "Jun"){
+    if (mes == "Jun")
+    {
         return 6;
     }
-    if (mes == "Jul"){
+    if (mes == "Jul")
+    {
         return 7;
     }
-    if (mes == "Aug"){
+    if (mes == "Aug")
+    {
         return 8;
     }
-    if (mes == "Sep"){
+    if (mes == "Sep")
+    {
         return 9;
     }
-    if (mes == "Oct"){
+    if (mes == "Oct")
+    {
         return 10;
     }
-    if (mes == "Nov"){
+    if (mes == "Nov")
+    {
         return 11;
     }
-    if (mes == "Dic"){
+    if (mes == "Dic")
+    {
         return 12;
     }
     return 0;
 }
 
-int sumaCodigo(string renglon){
+int sumaCodigo(string renglon)
+{
     string nombre;
     int mes, dia, hora, min, seg, n;
-    nombre = renglon.substr(0,3);
+    nombre = renglon.substr(0, 3);
     mes = numeroMes(nombre);
-    dia = stoi(renglon.substr(4,2));
-    if (dia > 9){
+    dia = stoi(renglon.substr(4, 2));
+    if (dia > 9)
+    {
         n = 1;
     }
-    else{
+    else
+    {
         n = 0;
     }
-    hora = stoi(renglon.substr(6,3));
-    min = stoi(renglon.substr(9+n,3));
-    seg = stoi(renglon.substr(12+n,3));
-    return mes*100000000 + dia*1000000 + hora*10000 + min*100 + seg;
+    hora = stoi(renglon.substr(6, 3));
+    min = stoi(renglon.substr(9 + n, 3));
+    seg = stoi(renglon.substr(12 + n, 3));
+    return mes * 100000000 + dia * 1000000 + hora * 10000 + min * 100 + seg;
 }
 
 // Función principal para ordenar el vector y guardar en un archivo de texto
-void MergeyGuarda(const vector<int>& meses, const vector<int>& dias, const string& filename) {
+void MergeyGuarda(const vector<int> &meses, const vector<int> &dias, const string &filename)
+{
     // Hacer una copia del vector de entrada para no modificarlo
     vector<int> dia = dias;
     vector<int> mes = meses;
@@ -147,8 +175,6 @@ void MergeyGuarda(const vector<int>& meses, const vector<int>& dias, const strin
     // Ordenar el vector utilizando MergeSort
     mergeSort(dia, 0, dia.size() - 1);
     mergeSort(mes, 0, mes.size() - 1);
-    
-
 
     // Crea un archivo de texto
     ofstream nuevaBitacora(filename);
@@ -162,76 +188,113 @@ void MergeyGuarda(const vector<int>& meses, const vector<int>& dias, const strin
     {
         cout << "Archivo creado correctamente." << endl;
     }
-    for (const int& num : mes) {
+    for (const int &num : mes)
+    {
         nuevaBitacora << num << " ";
     }
 
     // Escribir los elementos ordenados en el archivo de texto
-    for (const int& num : dia) {
+    for (const int &num : dia)
+    {
         nuevaBitacora << num << " ";
     }
 
     // Cerrar el archivo de texto
     nuevaBitacora.close();
-
 }
 
-int main()
-{
+void ordenaArchivo(){
+// decidimos ordenarlo con bubblesort que si bien no es muy eficiente, funciona y es lo unico que se pide
     // Abre un archivo para lectura
-    ifstream archivo("bitacoraCorta.txt");
+    ifstream archivo("bitacora.txt");
+    vector<string> renglones;
+    string linea;
+
 
     // Verifica si el archivo se abrió correctamente
     if (!archivo.is_open())
     {
-        cerr << "No se pudo abrir el archivo." << std::endl;
-        return 1;
+        cerr << "No se pudo abrir el archivo." << endl;
     }
     else
     {
         cout << "Archivo abierto correctamente." << endl;
     }
+            while (getline(archivo, linea))
+            {
+                renglones.push_back(linea);
+            }
+            int sizR = renglones.size();
+            for (int i = 0; i < sizR; i++)
+            {
+                for (int j = 0; j < sizR - 1 - i; j++)
+                {
+                    if (sumaCodigo(renglones[j]) > sumaCodigo(renglones[j + 1]))
+                    {
+                        renglones[j].swap(renglones[j + 1]);
+                    }
+                }
+            }
+
+            ofstream archivo1("nuevaBitacora.txt");
+
+            for (int i = 0; i < sizR; i++)
+            {
+                archivo1 << renglones[i] << endl;
+            }
+
+            archivo1.close();
+
+            // procedemos a ordenar el vector de enteros con el algoritmo de ordenamiento mergesort
+            // MergeyGuarda(meses,dias, "nuevaBitacora.txt");
+
+            cout << "Archivo creado y contenido escrito exitosamente." << endl;
+
+            // Cierra el archivo al finalizar
+            archivo.close();
+        }
+        
+
+int main()
+{
+
 
     // Lee el archivo línea por línea y almacena los datos en un vector de enteros de los dias
-    string linea;
     int dia;
     string mes;
     vector<int> dias;
     vector<int> meses;
-    vector<string> renglones;
+    vector<int> renglonesNumero;
+    string linea;
+   
 
+    ifstream archivo2("nuevaBitacora.txt");
 
-    //decidimos ordenarlo con bubblesort que si bien no es muy eficiente, funciona y es lo unico que se pide
-    while (getline(archivo, linea))
+    if (archivo2.is_open())
     {
-        renglones.push_back(linea);
-    }
-    int sizR = renglones.size();
-    for (int i = 0; i < sizR ; i++){
-        for (int j = 0; j < sizR - 1 - i; j++){
-            if (sumaCodigo(renglones[j]) > sumaCodigo(renglones[j+1])){
-                renglones[j].swap(renglones[j+1]);
-            }
+        cout << "El archivo ya existe" << endl;
+        for (int i = 0; i < 4; i++)
+        {
+            getline(archivo2, linea);
+            renglonesNumero.push_back(sumaCodigo(linea));
+        }
+
+        if (renglonesNumero[0] < renglonesNumero[1] && renglonesNumero[1] < renglonesNumero[2] && renglonesNumero[2] < renglonesNumero[3])
+        {
+            cout << "El archivo ya esta ordenado" << endl;
+        }
+        else {
+            cout << "El archivo no esta ordenado" << endl;
+            ordenaArchivo();
+
         }
     }
-
-    ofstream archivo1("nuevaBitacora1.txt");
-
-    for (int i = 0; i < sizR; i++){
-        archivo1<<renglones[i]<<endl;
+    else {
+        cout << "El archivo no existe" << endl;
+        ordenaArchivo();
     }
 
-    archivo1.close();
-
-    // procedemos a ordenar el vector de enteros con el algoritmo de ordenamiento mergesort
-    // MergeyGuarda(meses,dias, "nuevaBitacora.txt");
-
-
-
-    cout << "Archivo creado y contenido escrito exitosamente." << endl;
-
-    // Cierra el archivo al finalizar
-    archivo.close();
-
-    return 0;
-}
+            
+    
+        return 0;
+    }
