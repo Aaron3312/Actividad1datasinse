142f6ae1b22b20b49716ef9d98de7ce7fbc7e5ea
Aaron3312 <a01642529@tec.mx>
2024-03-17 21:55:43 -0600

    exmanmetodos

--- PruebasHuskle/pruebas1Act/nico30.hs
+++ PruebasHuskle/pruebas1Act/nico30.hs
@@ -1,27 +1,65 @@
-import Data.Bits (And(And))
+import Data.Bits (And (And))
+import Control.Monad.RWS.Class (MonadState(put))
 
-sumTotal x = if x == 0
+sumTotal x =
+  if x == 0
     then 0
     else x + sumTotal (x - 1)
 
-
 sumaTotalOpt :: (Eq t, Num t) => t -> t -> t
-sumaTotalOpt x y = if x == 0
+sumaTotalOpt x y =
+  if x == 0
     then y
     else sumaTotalOpt (x - 1) (x + y)
 
-
 -- crear lista recursiva
-crearLista :: (Eq t, Num t) => t -> [t]
-crearLista x = if x == 0
-    then []
-    else x : crearLista (x - 1)
+crearLista x y =
+  if x == 0
+    then y
+    else crearLista (x - 1) (z : y)
+  where
+    z = x - 1
 
---main
+duplicarLista :: [Int] -> [Int]
+duplicarLista = map (* 2)
+
+filterParesLista = filter (\x -> (mod x 2) == 0)
+
+oksucc x = succ x
+
+incrementarLista = map succ
+
+sumarTresNumeros x y z = x + y + z
+
+sumarYMostrar driver x y  = driver (show(x + y))
+
+sumaYImprime = sumarYMostrar putStrLn 
+
+
+combinar = incrementarLista . duplicarLista . filterParesLista
+
+--pattern matching
+--sealed classes 
+
+printLuckNum 7 = "Lucky number!!"
+printLuckNum 5 = "Lucky number!!"
+printLuckNum x = "Not lucky number"
+
+sumaTotalOpt1 :: (Eq t, Num t) => t -> t -> t
+sumaTotalOpt1 0 y = y
+sumaTotalOpt1 x y = sumaTotalOpt (x - 1) (x + y)
+
+--deconstruccion
+data Peso = PesoNKG Float | PesoNgramos Float
+
+mostrarPeso :: Peso -> IO ()
+mostrarPeso (PesoNgramos x) = putStrLn (show (x/1000))
+mostrarPeso (PesoNKG x) = putStrLn (show x)
+
+
+
+-- main
 main :: IO ()
 main = do
-    print (sumTotal 10)
-    print (sumaTotalOpt 10 0)
-    print (crearLista 10)
-
-    
+  print (sumTotal 10)
+  print (sumaTotalOpt 10 0)
---
+++ PruebasHuskle/pruebas1Act/pruebasYT/ActivClase.hs
@@ -0,0 +1,10 @@
+--crear una funcion que multiplique por 3 el argumento que le pase
+
+-- Solution: This is a simple program that asks for a number and then prints the number and the square of the number.
+
+multi3 :: Int -> Int
+multi3 x = x*3
+
+
+
+-- Call to print the input and output:
